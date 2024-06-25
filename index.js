5bbf39efb5395d1a4bc8323ed84fc68d09230a44
Niko Massaras <23nmassaras@student.dasd.org>
2024-06-25 10:21:55 -0400

    Revert "Based weather api works!"
    This reverts commit 9d1fc0ec147f63dd9f70a6885e7e392a1f06ee2b.

--- index.js
+++ index.js
@@ -3,17 +3,3 @@ const btn = document.querySelector('button');
 btn.onclick = () => {
     alert('This Button Works!')
 }
-
-
-window.addEventListener("load", (event) => {
-    console.log("page is fully loaded")
-    fetch("http://api.weatherapi.com/v1/current.json?key=fa18f4945881479d824160246241406&q=37.91822293084342, 21.745074031336777&aqi=no").then(resp=>resp.json()).then(data=>{
-        console.log(data)
-        const current = data.current.temp_c
-        console.log(current)
-        document.getElementById("weatherloc").textContent=current+" C!";
-    })
-
-
-  });
-
